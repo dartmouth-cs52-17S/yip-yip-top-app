895341d08f9352ada2a44ddce36e003040a35569
Byrne Hollander <bhollander823@gmail.com>
2017-05-15 13:47:09 -0400

    rename to YipYip

--- android/app/src/main/java/com/yipyip/MainApplication.java
+++ android/app/src/main/java/com/yipyip/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pepperoniapptemplate;
+package com.yipyip;
 
 import android.app.Application;
 
--- ios/YipYipTests/YipYipTests.m
+++ ios/YipYipTests/YipYipTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 180
 #define TEXT_TO_LOOK_FOR @"Increment counter"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface YipYipTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation YipYipTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
