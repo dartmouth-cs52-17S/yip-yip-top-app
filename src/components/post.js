52dc215a919f658c32ef3a536a64902cb05e08b5
YingLiugh <ying.liu.18@dartmouth.edu>
2017-05-22 19:02:33 -0400

    workin on post

---
+++ src/components/post.js
@@ -0,0 +1,21 @@
+/*
+import React, { Component } from 'react';
+class Post extends Component {
+  render() {
+    const video = this.props.video;
+    const description = video.snippet.description || '';
+    const vidId = video.id.videoId;
+    return (
+      <WebView
+          style={styles.frame}
+          source={{uri: `https://www.youtube.com/watch?v=${vidId}`}}
+          renderLoading={this.renderLoading}
+          renderError={this.renderError}
+          automaticallyAdjustContentInsets={false}
+      />
+    );
+  }
+}
+
+module.exports = Post;
+*/
