92457c8bd0fdb742c394e4b0b784357c7d33a688
ArminMahban <armin.mahban@gmail.com>
2017-05-22 17:34:30 -0400

    basic segmentedcontrolios

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,60 +1,42 @@
 import React, { Component } from 'react';
 
 import {
-  StyleSheet,
-  Text,
   View,
-  Button
+  Button,
+  SegmentedControlIOS
 } from 'react-native';
 
 
 class Feed extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedTab: 0,
+    }
   }
 
   showModal() {
-    console.log(this.props.navigation);
     this.props.navigation.navigate('Settings')
   }
 
   render() {
-    console.log("in render");
     return (
-      <View style={styles.container}>
+      <View>
+        <SegmentedControlIOS
+          values={['New', 'Top', 'Comments']}
+          selectedIndex={0}
+          tintColor={'#D6573D'}
+          onValueChange={(val) => {
+            console.log(`changed tab to ${val}`);
+            this.setState({
+              selectedTab: val
+            })
+        }} />
         <Button title="Show modal" onPress={() => this.props.navigation.navigate('Settings')} />
-        <Text style={styles.welcome}>
-          Welcome to FeedView!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-module.exports = Feed;
+export default Feed;
