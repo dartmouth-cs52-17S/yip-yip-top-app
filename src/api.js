2fcadba9d51d90f9595dbfc4c79d1027b2c1256e
YingLiugh <ying.liu.18@dartmouth.edu>
2017-05-23 15:16:10 -0400

    added createPost api

--- src/api.js
+++ src/api.js
@@ -5,14 +5,26 @@ const ROOT_URL = 'https://yip-yip.herokuapp.com/api';
 
 // yip-yip.herokuapp.com/api/posts/?long=5.000001&lat=6.000001
 
-export function fetchPosts(long, lat, callback) {
+export function fetchPosts(long, lat, cb) {
   const url = `${ROOT_URL}/posts/`;
   axios.get(url, {params: {long, lat}}).
   then((response) => {
     console.log(response.data);
-    callback(response.data);
+    cb(response.data);
   }).catch((error) => {
     console.log(error);
 
-  })
+  });
+}
+
+export function createPost(text, tags, coordinates, user_id, cb) {
+  const url = `${ROOT_URL}/posts/`;
+  axios.post(url, {params: {text, tags, coordinates, user_id}}).
+  then((response) => {
+    console.log(response.data);
+    cb(response.data);
+  }).catch((error) => {
+    console.log(error);
+
+  });
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-
 import {
-  Text,
-  View
+    Text,
+    View
 } from 'react-native';
-import styles from '../styles';
-
 class Post extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      error: false,
+      text: '',
+      score: 0,
+      tags: [],
+      location:'',
+      timestamp:'',
+    }
+    this.fetchPost = this.fetchPost.bind(this);
   }
 
+  componentWillMount() {
+    fetchPost(this.props._id);
+  }
+  fetchPost(id) {
+    axios.get(`${ROOT_URL}/posts/${id}`).then((response) => {
+      this.setState({
+        text: response.data.text,
+        score: response.data.score,
+        timestamp: response.data.timestamp,
+        tags: response.data.tags,
+        location: response.data.location,
+      });r
+    }).catch((error) => {
+      this.setState({
+        error: true,
+      });
+    });
+  }
   render() {
     console.log('in render');
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Posts page!
+      <View>
+        <Text>
+          This is the detail post.
         </Text>
       </View>
     );
