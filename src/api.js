a5d8808a17f25ab7dc3ae79ed9162714d905b059
YingLiugh <ying.liu.18@dartmouth.edu>
2017-05-23 16:23:13 -0400

    added edit post api & PostDetail component

--- src/api.js
+++ src/api.js
@@ -30,7 +30,7 @@ export function createPost(text, tags, coordinates, user_id, cb) {
 }
 
 export function getPost(post_id, cb) {
-  const url = `${ROOT_URL}/post/${post_id}`;
+  const url = `${ROOT_URL}/posts/${post_id}`;
   axios.get(url, {params: {post_id}}).
   then((response) => {
     console.log(response.data);
@@ -42,8 +42,19 @@ export function getPost(post_id, cb) {
 }
 
 export function deletePost(post_id, cb) {
-  const url = `${ROOT_URL}/post/${post_id}`;
+  const url = `${ROOT_URL}/posts/${post_id}`;
   axios.delete(url, {params: {post_id}}).
+  then((response) => {
+    console.log(response.data);
+    cb(response.data);
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+export function editPost(post,fields,cb) {
+  const url = `${ROOT_URL}/posts/${post._id}`;
+  axios.put(url, {params: {id: post._id, fields}}).
   then((response) => {
     console.log(response.data);
     cb(response.data);
