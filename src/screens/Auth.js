62c1f91f09973addd74bca8e3be24034855f41be
Byrne Hollander <bhollander823@gmail.com>
2017-05-27 15:52:18 -0400

    persisting profile and token to storage

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
 import Auth0Lock from 'react-native-lock';
 
 class Auth extends React.Component {
+
+  async saveLogin(profile, token) {
+    try {
+      await AsyncStorage.setItem('@Profile:key', JSON.stringify(profile));
+      await AsyncStorage.setItem('@HerdLong:key', JSON.stringify(token));
+      console.log(`profile is ${JSON.stringify(profile)}`);
+      console.log(`token is ${JSON.stringify(token)}`);
+    } catch (error) {
+      console.log(`Count not save login. ${error}`);
+    }
+  }
 
   render() {
     var lock = new Auth0Lock({clientId: 'z84JtDyqbr7VldTci4QupQaaD9akB0rT', domain: 'bhollander823.auth0.com'});
@@ -15,10 +26,7 @@ class Auth extends React.Component {
             console.log(err);
             return;
           }
-          // Authentication worked!
-          console.log('Logged in with Auth0!');
-          console.log(`profile is ${JSON.stringify(profile)}`);
-          console.log(`token is ${JSON.stringify(token)}`);
+          this.saveLogin(profile, token);
         })}
       </View>
     );
