d55907a62beb0d59d788bafaefe04f2411346a80
ArminMahban <armin.mahban@gmail.com>
2017-05-28 20:03:39 -0400

    rounded comments

--- src/screens/Comment.js
+++ src/screens/Comment.js
@@ -31,31 +31,31 @@ class Comment extends Component {
     }
     const time = timeSince.join(' ');
     return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', marginLeft: 17, marginRight: 17, marginTop: 10}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', marginLeft: 17, marginRight: 17, marginTop: 10, backgroundColor: 'white', borderRadius: 10}}>
 
-          <View style={{flex: 4, backgroundColor: '#FFFFFF', flexDirection: 'row'}}>
+            <View style={{flex: 4, backgroundColor: 'transparent', flexDirection: 'row'}}>
 
-            <View style={{flex: 1, justifyContent: 'center', backgroundColor: '#FFFFFF'}}>
-              <Icon
-                reverse
-                name='ios-american-football'
-                type='ionicon'
-                color='#517fa4'
-              />
+              <View style={{flex: 1, justifyContent: 'center'}}>
+                <Icon
+                  reverse
+                  name='ios-american-football'
+                  type='ionicon'
+                  color='#517fa4'
+                />
+              </View>
+              <View style={{flex: 3, justifyContent: 'center'}}>
+                <Text> {comm.text} </Text>
+                <Text> {time} </Text>
+              </View>
+
             </View>
-            <View style={{flex: 3, justifyContent: 'center', backgroundColor: '#FFFFFF'}}>
-              <Text> {comm.text} </Text>
-              <Text> {time} </Text>
-            </View>
 
+          <View style={{flex: 1, alignItems: 'center'}}>
+            <Icon type="ionicon" name='ios-arrow-up' size={35} color={(this.state.upvote? '#DA5AA4':'#6C56BA')} />
+            <Text> {comm.score} </Text>
+            <Icon type="ionicon" name='ios-arrow-down' size={35} color={(this.state.downvote? '#DA5AA4':'#6C56BA')} onPress={this.downVote}/>
           </View>
-
-        <View style={{flex: 1, alignItems: 'center',  backgroundColor: '#FFFFFF'}}>
-          <Icon type="ionicon" name='ios-arrow-up' size={35} color={(this.state.upvote? '#DA5AA4':'#6C56BA')} />
-          <Text> {comm.score} </Text>
-          <Icon type="ionicon" name='ios-arrow-down' size={35} color={(this.state.downvote? '#DA5AA4':'#6C56BA')} onPress={this.downVote}/>
         </View>
-      </View>
     );
   }
 }
