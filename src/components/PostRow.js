c38e5f4acae7d601c50171da2917f8938b643a4d
Byrne Hollander <bhollander823@gmail.com>
2017-06-02 11:03:16 -0400

    don't change 1 to a -- I feel like this fixed a while ago?

--- src/components/PostRow.js
+++ src/components/PostRow.js
@@ -96,7 +96,7 @@ class PostRow extends Component {
   render() {
     let timeSince = moment(this.props.post.timestamp).fromNow().split(' ');
     timeSince.splice(-1,1);
-    if (timeSince[0] === 'an' || timeSince[0] === 'a') {
+    if (timeSince[0] === 'an') {
       timeSince[0] = '1'
     }
     if (timeSince[1] === 'minutes') {
